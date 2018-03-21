dce14147ab7ed3276bcae930e8e129920cd43eec
INW-TECH\joao.psimoes <joao.psimoes@innowave.tech>
2018-03-21 12:40:00 +0000

    test

---
+++ ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,46 @@
+﻿using ContactManager.Models;
+using ContactManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        /// <summary>
+        /// Get contacts from cache
+        /// </summary> 
+        /// <returns> return list of contacts </returns>
+        public Contact[] Get()
+        {
+          return  contactRepository.GetAllContacts();
+
+        }
+
+        /// <summary>
+        /// Add new contact to cache
+        /// </summary>
+        /// <param name="contact"> Contact (ID:int; Name:string)</param>
+        /// <returns> HTTP Response</returns>
+      
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+            return response;
+
+        }
+          
+    }
+}
---
+++ UnitTestContactManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestContactManager")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestContactManager")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e2e86759-d9d0-4d1d-bc83-852121e57693")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
