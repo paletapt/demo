b88c772652366607bbf77bf364e1d1025f836a1f
INW-TECH\joao.psimoes <joao.psimoes@innowave.tech>
2018-03-25 16:24:08 +0100

    a

---
+++ New folder/src/Fiver.Mvc.Testing/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Fiver.Mvc.Testing.OtherLayers;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Fiver.Mvc.Testing
+{
+    public class Startup
+    {
+        public void ConfigureServices(
+            IServiceCollection services)
+        {
+            // for integration testing use Try...
+            services.TryAddSingleton<IMovieService, MovieService>(); 
+            
+            services.AddMvc();
+        }
+
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
