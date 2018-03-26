77e8e7665d29d29d9574fd4538c88c98185bd69e
INW-TECH\joao.psimoes <joao.psimoes@innowave.tech>
2018-03-26 09:51:34 +0100

    remove folder

--- New folder/src/Fiver.Mvc.Testing/Models/Home/MovieInfoViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fiver.Mvc.Testing.Models.Home
-{
-    public class MovieInfoViewModel
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public int ReleaseYear { get; set; }
-    }
-}
